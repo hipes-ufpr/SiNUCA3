0f86dd3ef553eaf360f3b72f47a41c98522da826
fbc23 <fernandocuritiba3@gmail.com>
2025-02-24 21:02:26 -0300

    Optimazations in pintool

--- src/trace_generator/sinuca3_pintool.cpp
+++ src/trace_generator/sinuca3_pintool.cpp
@@ -1,15 +1,11 @@
 #include "pin.H"
 #include "../utils/logging.hpp"
 #include "../trace_reader/orcs_trace_reader.hpp"
-#include "types_base.PH"
-#include "types_vmapi.PH"
+#include <cstddef>
 #include <cstdio>
 #include <cstdlib>
 #include <cstring>
 #include <string>
-#ifdef debug
-#include <iostream> // REMOVE
-#endif
 
 #define BUFFER_SIZE 1048576
 #define BBL_ID_BYTE_SIZE 2
@@ -19,18 +15,25 @@
 #define MEMWRITE_SIZE IARG_MEMORYWRITE_SIZE
 #define MEMREAD2_EA IARG_MEMORYREAD2_EA
 
-// class definitions
-class buffer {
+struct DataINS {
+    ADDRINT addr;
+    char size;
+    REG baseReg;
+    REG indexReg;
+    char byte;
+};
+
+class Buffer {
   public:
     char store[BUFFER_SIZE];
     size_t numUsedBytes;
     size_t minimumSpace;
 
-    buffer(size_t min) {
+    Buffer(size_t min) {
         numUsedBytes = 0;
         this->minimumSpace = min;
     }
-    ~buffer() {};
+    ~Buffer() {};
     inline void loadBufToFile(FILE* file) {
         size_t written = fwrite(this->store, 1, this->numUsedBytes, file);
         if (written < this->numUsedBytes) {SINUCA3_ERROR_PRINTF("Buffer error");}
@@ -45,7 +48,7 @@ class buffer {
 KNOB<INT> KnobNumberIns(KNOB_MODE_WRITEONCE, "pintool", "number_max_inst", 
                         "-1", "Maximum number of instructions to be traced");
 FILE *staticTrace, *memoryTrace, *dynamicTrace;
-buffer *staticBuffer, *memoryBuffer, *dynamicBuffer;
+Buffer *staticBuffer, *memoryBuffer, *dynamicBuffer;
 static bool isInstrumentationOn;
 static const size_t ADDR_SIZE = sizeof(ADDRINT);
 
@@ -71,14 +74,15 @@ VOID initInstrumentation() {
     fseek(staticTrace, 4, SEEK_SET);
 }
 
-VOID stopInstrumentation(int bblCount) {
+VOID stopInstrumentation(unsigned int bblCount) {
     SINUCA3_LOG_PRINTF("End of tool instrumentation\n");
+    SINUCA3_DEBUG_PRINTF("Number of BBLs => %u\n", bblCount);
     isInstrumentationOn = false;
 
     size_t savedPosition = ftell(staticTrace);
-    fseek(staticTrace, 0, SEEK_SET);
-    fwrite((void*)&bblCount, 1, sizeof(bblCount), staticTrace);
-    fseek(staticTrace, savedPosition, SEEK_SET);
+    std::rewind(staticTrace);
+    std::fwrite((void*)&bblCount, 1, sizeof(bblCount), staticTrace);
+    std::fseek(staticTrace, savedPosition, SEEK_SET);
 
     if (staticBuffer->numUsedBytes>0) {
         staticBuffer->loadBufToFile(staticTrace);
@@ -91,64 +95,45 @@ VOID stopInstrumentation(int bblCount) {
     }
 }
 
+inline VOID copy(char* buf, size_t* used, void* src, size_t size) {
+    std::memcpy(buf+*used, src, size);
+    (*used)+=size;
+}
+
 VOID x86ToStaticBuf(INS* ins) {
-    char* buf = staticBuffer->store;
-    size_t* used = &staticBuffer->numUsedBytes;
     namespace orcs = sinuca::traceReader::orcsTraceReader;
+    static char* buf = staticBuffer->store;
+    static size_t* used = &staticBuffer->numUsedBytes;
+    static DataINS data;
+    static orcs::Branch bT;
+    static bool flag;
 
-    // get mnemonic
     std::string name = INS_Mnemonic(*ins);
-    std::memcpy(buf+*used, name.c_str(), name.size());
-    (*used)+=name.size();
-    // get address
-    ADDRINT addr = INS_Address(*ins);
-    std::memcpy(buf+*used, (void*)&addr, sizeof(addr));
-    (*used)+=sizeof(addr);
-    // get size
-    char size = static_cast<char>(INS_Size(*ins));
-    std::memcpy(buf+*used, (void*)&size, sizeof(size));
-    (*used)+=sizeof(size);
-    // get memory base reg
-    REG baseReg = INS_MemoryBaseReg(*ins);
-    std::memcpy(buf+*used, (void*)&baseReg, sizeof(baseReg));
-    (*used)+=sizeof(baseReg);
-    // get memory index reg
-    REG indexReg = INS_MemoryIndexReg(*ins);
-    std::memcpy(buf+*used, (void*)&indexReg, sizeof(indexReg));
-    (*used)+=sizeof(indexReg);
-    
-    // conversion from bool to char may only guarantee one bit 0 or 1
-    // the rest being crap
-    char byte=0;
-    byte |= INS_IsPredicated(*ins) ? 0x1 : 0x0;
-    byte |= ((INS_IsPrefetch(*ins) ? 0x1 : 0x0) >> 1);
+    data.addr = INS_Address(*ins);
+    data.size = static_cast<char>(INS_Size(*ins));
+    data.baseReg = INS_MemoryBaseReg(*ins);
+    data.indexReg = INS_MemoryIndexReg(*ins);
+    data.byte = 0;
 
-    bool isBranch = (INS_IsControlFlow(*ins) || INS_IsSyscall(*ins));
-    orcs::Branch branchType;
-    if (isBranch == true) {
-        byte |= (0x1 >> 2);
-        byte |= ((INS_IsIndirectControlFlow(*ins) ? 0x1 : 0x0) >> 3);
-        if (INS_IsCall(*ins)) {
-            branchType = orcs::BranchCall;
-        } else if (INS_IsRet(*ins)) {
-            branchType = orcs::BranchReturn;
-        } else if (INS_IsSyscall(*ins)) {
-            branchType = orcs::BranchSyscall;
-        } else if (INS_HasFallThrough(*ins)) {
-            branchType = orcs::BranchCond;
-        } else {
-            branchType = orcs::BranchUncond;
-        }
+    if (INS_IsPredicated(*ins)) {data.byte |= 1;}
+    if (INS_IsPrefetch(*ins)) {data.byte |= 1 >> 1;}
+    if ((flag = INS_IsCall(*ins))) {
+        bT = orcs::BranchCall; 
+    } else if ((flag = INS_IsRet(*ins))) {
+        bT = orcs::BranchReturn; 
+    } else if ((flag = INS_IsSyscall(*ins))) {
+        bT = orcs::BranchSyscall; 
+    } else if ((flag = INS_IsControlFlow(*ins))) {
+        if (INS_IsIndirectControlFlow(*ins)) {data.byte |= 1 >> 3;}
+        bT = INS_HasFallThrough(*ins) ? orcs::BranchCond : orcs::BranchUncond;
     }
-    
-    std::memcpy(buf+*used, (void*)&byte, sizeof(byte));
-    (*used)+=sizeof(byte);
 
-    if (isBranch == true) {
-        char branchTypeByte = static_cast<char>(branchType);
-        std::memcpy(buf+*used, (void*)&branchTypeByte, sizeof(branchTypeByte));
-        (*used)+=sizeof(branchTypeByte);
+    copy(buf, used, (void*)name.c_str(), name.size()+1);
+    if (flag == true) {
+        data.byte |= 1 >> 2;
+        copy(buf+sizeof(data), used, (void*)&bT, 1);
     }
+    copy(buf-1, used, (void*)&data, sizeof(data));
 
     if (staticBuffer->isBufFull() == true) {
         staticBuffer->loadBufToFile(staticTrace);
@@ -159,10 +144,8 @@ VOID appendToMemoryTrace(ADDRINT addr, INT32 size) {
     char* buf = memoryBuffer->store;
     size_t* used = &memoryBuffer->numUsedBytes;
 
-    std::memcpy(buf+*used, (void*)&addr, sizeof(addr));
-    (*used)+=sizeof(addr);
-    std::memcpy(buf+*used, (void*)&size, sizeof(size));
-    (*used)+=sizeof(size);
+    copy(buf, used, (void*)&addr, sizeof(addr));
+    copy(buf, used, (void*)&size, sizeof(size));
     if (memoryBuffer->isBufFull() == true) {
         memoryBuffer->loadBufToFile(memoryTrace);
     }
@@ -188,7 +171,7 @@ VOID instrumentMem(INS* ins) {
 VOID trace(TRACE trace, VOID *ptr) {
     char* buf = staticBuffer->store;
     size_t *usedStatic=&staticBuffer->numUsedBytes, bblInit;
-    static int bblCount = 1;
+    static unsigned int bblCount = 0;
     int numInstBbl;
     
     if (isInstrumentationOn == true) {
@@ -200,15 +183,17 @@ VOID trace(TRACE trace, VOID *ptr) {
     if (isInstrumentationOn == false) {return;}
 
     for (BBL bbl = TRACE_BblHead(trace); BBL_Valid(bbl); bbl = BBL_Next(bbl)) {
-        BBL_InsertCall(bbl, IPOINT_BEFORE, (AFUNPTR)appendToDynamicTrace, IARG_UINT32, bblCount, IARG_END);
+        BBL_InsertCall(bbl, IPOINT_BEFORE, (AFUNPTR)appendToDynamicTrace, 
+                      IARG_UINT32, bblCount, IARG_END);
+        bblCount++;
         numInstBbl = 0;
         bblInit = (*usedStatic)++;
-        for (INS ins = BBL_InsHead(bbl); INS_Valid(ins); ins = INS_Next(ins), numInstBbl++) {
+        for (INS ins = BBL_InsHead(bbl); INS_Valid(ins); ins = INS_Next(ins)) {
+            numInstBbl++;
             x86ToStaticBuf(&ins);
             instrumentMem(&ins);
         }
         std::memcpy(buf+bblInit, (void*)&numInstBbl, 1);
-        bblCount++;
     }
 
     return;
@@ -222,11 +207,11 @@ VOID imageLoad(IMG img, VOID* ptr) {
     size_t it = nameImg.find_last_of('/')+1;
     const char* subStr = &nameImg.c_str()[it];
 
-    snprintf(fileName, sizeof(fileName)-1, "static_%s.trace", subStr);
+    snprintf(fileName, sizeof(fileName), "static_%s.trace", subStr);
     staticTrace = fopen(fileName, "wb");
-    snprintf(fileName, sizeof(fileName)-1, "dynamic_%s.trace", subStr);
+    snprintf(fileName, sizeof(fileName), "dynamic_%s.trace", subStr);
     dynamicTrace = fopen(fileName, "wb");
-    snprintf(fileName, sizeof(fileName)-1, "memory_%s.trace", subStr);
+    snprintf(fileName, sizeof(fileName), "memory_%s.trace", subStr);
     memoryTrace = fopen(fileName, "wb");
 
     for (SEC sec = IMG_SecHead(img); SEC_Valid(sec); sec = SEC_Next(sec)) {
@@ -257,9 +242,9 @@ int main(int argc, char* argv[]) {
         return usage();
     }
     
-    staticBuffer = new buffer(64);
-    dynamicBuffer = new buffer(BBL_ID_BYTE_SIZE);
-    memoryBuffer = new buffer(sizeof(ADDRINT)+sizeof(INT32));
+    staticBuffer = new Buffer(64);
+    dynamicBuffer = new Buffer(BBL_ID_BYTE_SIZE);
+    memoryBuffer = new Buffer(sizeof(ADDRINT)+sizeof(INT32));
     isInstrumentationOn = false;
 
     IMG_AddInstrumentFunction(imageLoad, NULL);
---
+++ src/trace_reader/sinuca3_trace_reader.hpp
@@ -0,0 +1,160 @@
+#ifndef SINUCA3_ORCS_TRACE_READER_HPP
+#define SINUCA3_ORCS_TRACE_READER_HPP
+
+//
+// Copyright (C) 2024  HiPES - Universidade Federal do Paraná
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+//
+
+/**
+ * @file orcs_trace_reader.hpp
+ * @brief Port of the OrCS trace reader. https://github.com/mazalves/OrCS
+ */
+
+#include <stdint.h>  // uint32_t
+#include <cstdio>
+
+#include <cstring>
+
+#include "trace_reader.hpp"
+
+const int TRACE_LINE_SIZE = 512;
+
+namespace sinuca {
+namespace traceReader {
+namespace sinuca3TraceReader {
+
+/** @brief Enumerates the INSTRUCTION (Opcode and Uop) operation type. */
+enum InstructionOperation {
+    // NOP
+    InstructionOperationNop,
+    // INTEGERS
+    InstructionOperationIntAlu,
+    InstructionOperationIntMul,
+    InstructionOperationIntDiv,
+    // FLOAT POINT
+    InstructionOperationFPAlu,
+    InstructionOperationFPMul,
+    InstructionOperationFPDiv,
+    // BRANCHES
+    InstructionOperationBranch,
+    // MEMORY OPERATIONS
+    InstructionOperationMemLoad,
+    InstructionOperationMemStore,
+    // NOT IDENTIFIED
+    InstructionOperationOther,
+    // SYNCHRONIZATION
+    InstructionOperationBarrier,
+    // HMC
+    InstructionOperationHMCROA,  // #12 READ+OP +Answer
+    InstructionOperationHMCROWA  // #13 READ+OP+WRITE +Answer
+};
+
+/** @brief Enumerates the types of branches. */
+enum Branch {
+    BranchSyscall,
+    BranchCall,
+    BranchReturn,
+    BranchUncond,
+    BranchCond
+};
+
+struct OpcodePackage {
+    char opcodeAssembly[TRACE_LINE_SIZE];
+    uint64_t opcodeAddress;
+    uint32_t opcodeSize;
+
+    uint32_t readRegs[16];
+    uint32_t writeRegs[16];
+
+    uint32_t baseReg;
+    uint32_t indexReg;
+
+    bool isRead;
+    uint64_t readAddress;
+    uint32_t readSize;
+
+    bool isRead2;
+    uint64_t read2Address;
+    uint32_t read2Size;
+
+    bool isWrite;
+    uint64_t writeAddress;
+    uint32_t writeSize;
+
+    Branch branchType;
+    bool isIndirect;
+
+    bool isPredicated;
+    bool isPrefetch;
+
+    inline OpcodePackage() {
+        memset(this, 0, sizeof(*this));
+        memcpy(this->opcodeAssembly, "N/A", 4);
+        this->branchType = BranchUncond;
+    }
+};
+
+/** @brief Port of the trace reader from OrCS (a.k.a. SiNUCA2). */
+class SinucaTraceReader : public TraceReader {
+  private:
+    FILE *StaticTraceFile;
+    FILE *DynamicTraceFile;
+    FILE *MemoryTraceFile;
+
+    // Controls the trace reading.
+    bool isInsideBBL;
+    uint32_t currentBBL;
+    uint32_t currentOpcode;
+
+    // Controls the static dictionary.
+    // Total of BBLs for the static file.
+    // Total of instructions for each BBL.
+    unsigned int binaryTotalBBLs;
+    uint32_t *binaryBBLSize;
+    // Complete dictionary of BBLs and instructions.
+    OpcodePackage **binaryDict;
+
+    uint64_t fetchInstructions;
+
+    // Generate the static dictionary.
+    int GetTotalBBLs();
+    //int DefineBinaryBBLSize();
+    //int GenerateBinaryDict();
+
+    //int TraceStringToOpcode(char *input_string, OpcodePackage *opcode);
+    //int TraceNextDynamic(uint32_t *next_bbl);
+    //int TraceNextMemory(uint64_t *next_address, uint32_t *operation_size,
+                       // bool *is_read);
+
+    //FetchResult TraceFetch(OpcodePackage *m);
+
+  public:
+    virtual int OpenTrace(const char *traceFileName);
+    //virtual void PrintStatistics();
+    //virtual FetchResult Fetch(InstructionPacket *ret);
+
+    inline ~SinucaTraceReader() {
+        fclose(this->StaticTraceFile);
+        fclose(this->DynamicTraceFile);
+        fclose(this->MemoryTraceFile);
+    }
+};
+
+}  // namespace orcsTraceReader
+}  // namespace traceReader
+}  // namespace sinuca
+
+#endif  // SINUCA3_ORCS_TRACE_READER_HPP
