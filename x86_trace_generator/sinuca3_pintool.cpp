0e524bbdd3775d29b3c4499a336c35625da54c0c
ferbcastro <fernandocuritiba3@gmail.com>
2025-05-13 22:45:48 -0300

    Renamed dir to x86_trace_generator

--- x86_trace_generator/sinuca3_pintool.cpp
+++ x86_trace_generator/sinuca3_pintool.cpp
@@ -30,7 +30,7 @@ extern "C" {
 }
 
 #include "../src/utils/logging.hpp"
-#include "generator_file_handler.hpp"
+#include "./utils/generator_file_handler.hpp"
 
 const int MEMREAD_EA = IARG_MEMORYREAD_EA;
 const int MEMREAD_SIZE = IARG_MEMORYREAD_SIZE;
