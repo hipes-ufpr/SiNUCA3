2e687e3d6cc24ccc34f160d16d8e1bf891461e95
Vinicius <viniciusjere@outlook.com>
2025-08-13 18:54:56 -0300

    Helper Methods Implemented

---
+++ src/std_components/fetch/boom_fetch.cpp
@@ -0,0 +1,178 @@
+//
+// Copyright (C) 2025  HiPES - Universidade Federal do Paraná
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+//
+
+/**
+ * @file boom_fetch.cpp
+ * @brief Implementation of the Boom Fetch component.
+ */
+
+#include "boom_fetch.hpp"
+
+#include <cstddef>
+
+#include "config/config.hpp"
+#include "engine/component.hpp"
+#include "engine/default_packets.hpp"
+#include "std_components/predictors/interleavedBTB.hpp"
+#include "utils/logging.hpp"
+
+int BoomFetch::FetchConfigParameter(ConfigValue value) {
+    if (value.type == ConfigValueTypeComponentReference) {
+        this->fetch = dynamic_cast<Component<FetchPacket>*>(
+            value.value.componentReference);
+        if (this->fetch != NULL) {
+            return 0;
+        }
+    }
+    SINUCA3_ERROR_PRINTF(
+        "Boom Fetch parameter `fetch` is not a Component<FetchPacket>.\n");
+    return 1;
+}
+
+int BoomFetch::InstructionMemoryConfigParameter(ConfigValue value) {
+    if (value.type == ConfigValueTypeComponentReference) {
+        this->instructionMemory = dynamic_cast<Component<InstructionPacket>*>(
+            value.value.componentReference);
+        if (this->instructionMemory != NULL) {
+            return 0;
+        }
+    }
+    SINUCA3_ERROR_PRINTF(
+        "Boom Fetch parameter `instructionMemory` is not a "
+        "Component<InstructionPacket>.\n");
+    return 1;
+}
+
+int BoomFetch::BTBConfigParameter(ConfigValue value) {
+    if (value.type == ConfigValueTypeComponentReference) {
+        this->btb =
+            dynamic_cast<Component<BTBPacket>*>(value.value.componentReference);
+        if (this->btb != NULL) {
+            return 0;
+        }
+    }
+    SINUCA3_ERROR_PRINTF(
+        "Boom Fetch parameter `btb` is not a "
+        "Component<BTBPacket>.\n");
+    return 1;
+}
+
+int BoomFetch::RASConfigParameter(ConfigValue value) {
+    if (value.type == ConfigValueTypeComponentReference) {
+        this->ras = dynamic_cast<Component<PredictorPacket>*>(
+            value.value.componentReference);
+        if (this->ras != NULL) {
+            return 0;
+        }
+    }
+    SINUCA3_ERROR_PRINTF(
+        "Boom Fetch parameter `ras` is not a "
+        "Component<PredictorPacket>.\n");
+    return 1;
+}
+
+int BoomFetch::FetchSizeConfigParameter(ConfigValue value) {
+    if (value.type == ConfigValueTypeInteger) {
+        if (value.value.integer > 0) {
+            this->fetchSize = value.value.integer;
+            return 0;
+        }
+    }
+
+    SINUCA3_ERROR_PRINTF(
+        "Boom Fetch parameter `fetchSize` is not a integer > 0.\n");
+    return 1;
+}
+
+int BoomFetch::FetchIntervalConfigParameter(ConfigValue value) {
+    if (value.type == ConfigValueTypeInteger) {
+        if (value.value.integer > 0) {
+            this->fetchInterval = value.value.integer;
+            return 0;
+        }
+    }
+    SINUCA3_ERROR_PRINTF(
+        "Boom Fetch parameter `fetchInterval` is not a integer > 0.\n");
+    return 1;
+}
+
+int BoomFetch::MisspredictPenaltyConfigParameter(ConfigValue value) {
+    if (value.type == ConfigValueTypeInteger) {
+        if (value.value.integer > 0) {
+            this->misspredictPenalty = value.value.integer;
+            return 0;
+        }
+    }
+    SINUCA3_ERROR_PRINTF(
+        "Boom Fetch parameter `misspredictPenalty` is not a integer > 0.\n");
+    return 1;
+}
+
+int BoomFetch::FinishSetup() {
+    if (this->fetch == NULL) {
+        SINUCA3_ERROR_PRINTF(
+            "Boom Fetch didn't received required parameter `fetch`.\n");
+        return 1;
+    }
+
+    if (this->instructionMemory == NULL) {
+        SINUCA3_ERROR_PRINTF(
+            "Boom Fetch didn't received required parameter "
+            "`instructionMemory`.\n");
+        return 1;
+    }
+
+    if (this->btb == NULL) {
+        SINUCA3_ERROR_PRINTF(
+            "Boom Fetch didn't received required parameter `btb`.\n");
+        return 1;
+    }
+
+    if (this->ras == NULL) {
+        SINUCA3_ERROR_PRINTF(
+            "Boom Fetch didn't received required parameter `ras`.\n");
+        return 1;
+    }
+
+    this->fetchID = this->fetch->Connect(this->fetchSize);
+    this->instructionMemoryID =
+        this->instructionMemory->Connect(this->fetchSize);
+
+    this->fetchBuffer = new FetchBufferEntry[this->fetchSize];
+
+    return 0;
+}
+
+int BoomFetch::SetConfigParameter(const char* parameter, ConfigValue value) {
+    return 0;
+}
+
+void BoomFetch::Clock() {
+    // Implementation of Clock
+}
+
+void BoomFetch::Flush() {
+    // Implementation of Flush
+}
+
+void BoomFetch::PrintStatistics() {
+    // Implementation of PrintStatistics
+}
+
+BoomFetch::~BoomFetch() {
+    // Implementation of destructor
+}
