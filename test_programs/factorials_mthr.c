6c458c87b57cf82cedefbebee02b668574d4a0b4
ferbcastro <fernandocuritiba3@gmail.com>
2025-10-27 00:56:12 -0300

    multithread test refactor

--- test_programs/factorials_mthr.c
+++ test_programs/factorials_mthr.c
@@ -1,6 +1,8 @@
 #include <instrumentation_control.h>
 #include <omp.h>
 
+#include <string.h>
+
 int __attribute__((noinline)) IterativeFactorial(int x) {
     if (x == 0 || x == 1) {
         return 1;
@@ -22,36 +24,113 @@ int __attribute__((noinline)) RecursiveFactorial(int x) {
     return x * RecursiveFactorial(x - 1);
 }
 
-int main(void) {
+void TestOneThread() {
+#pragma omp parallel num_threads(1)
+    {
+        int a = 10;
+        a = IterativeFactorial(a);
+        int b = 5;
+        b = RecursiveFactorial(b);
+    }
+}
+
+void TestNestedParallelBlock() {
     omp_set_nested(1);
 
-    int arr[6];
+#pragma omp parallel
+    {
+#pragma omp parallel
+        {
+            int a = 10;
+            a = IterativeFactorial(a);
+            int b = 5;
+            b = RecursiveFactorial(b);
+        }
+    }
+
+    omp_set_nested(0);
+}
+
+void TestOmpBarrier() {
+#pragma omp parallel
+    {
+        int a = 10;
+        a = IterativeFactorial(a);
+#pragma omp barrier
+        int b = 5;
+        b = RecursiveFactorial(b);
+    }
+}
+
+void TestOmpGlobalCriticalBlock() {
+#pragma omp parallel
+    {
+        int a = 10;
+        int b = 5;
+#pragma omp critical
+        {
+            a = IterativeFactorial(a);
+            b = RecursiveFactorial(b);
+        }
+    }
+}
+
+void TestOmpNamedCriticalBlock() {
+#pragma omp parallel
+    {
+        int a = 10;
+        int b = 5;
+#pragma omp critical(crit)
+        {
+            a = IterativeFactorial(a);
+            b = RecursiveFactorial(b);
+        }
+    }
+}
+
+void TestOmpLock() {}
+
+void TestOmpSingleBlock() {}
+
+void TestOmpMasterBlock() {}
+
+#define TEST_SIZE 7
+
+void TestOmpFor() {
+    int arr[TEST_SIZE];
+
+#pragma omp parallel shared(arr)
+    {
+#pragma omp for
+        for (int i = 0; i < TEST_SIZE; ++i) {
+            arr[i] = IterativeFactorial(i);
+        }
+    }
+}
+
+#define TEST(func)                      \
+    if (!strcmp(test, #func)) {         \
+        EnableThreadInstrumentation();  \
+        func();                         \
+        DisableThreadInstrumentation(); \
+    }
+
+int main(int argc, char* argv[]) {
+    if (argc < 2) {
+        return 1;
+    }
+
+    char* test = argv[1];
 
     BeginInstrumentationBlock();
 
-#pragma omp parallel shared(arr) num_threads(1)
-    {
-
-#pragma omp for
-        for (int i = 0; i < 6; ++i) {
-            EnableThreadInstrumentation();
-            arr[i] = IterativeFactorial(i);
-            DisableThreadInstrumentation();
-        }
-    }
-
-#pragma omp parallel num_threads(1)
-    {
-        volatile int b = 5;
-#pragma omp critical(crit1)
-        {
-            EnableThreadInstrumentation();
-            b = RecursiveFactorial(b);
-            DisableThreadInstrumentation();
-        }
-
-#pragma omp barrier
-    }
+    TEST(TestOneThread)
+    TEST(TestNestedParallelBlock)
+    TEST(TestOmpGlobalCriticalBlock)
+    TEST(TestOmpNamedCriticalBlock)
+    TEST(TestOmpMasterBlock)
+    TEST(TestOmpBarrier)
+    TEST(TestOmpFor)
 
     EndInstrumentationBlock();
 
