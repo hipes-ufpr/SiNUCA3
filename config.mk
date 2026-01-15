87a07853e4c74664d8a9a97e5d4d6866b5d54970
Gabriel G. de Brito <me@gboncoffee.dev.br>
2026-01-15 16:14:10 +0000

    Allow the user to pass compilation arguments via _FLAGS

--- config.mk
+++ config.mk
@@ -1,8 +1,8 @@
 CC = clang
 CPP = clang++
 
-CFLAGS = -Wall -Wextra -std=c99 -I./src -g
-CPPFLAGS = -Wall -Wextra -fno-exceptions -std=c++98 -I./src -g
+CFLAGS += -Wall -Wextra -std=c99 -I./src -g
+CPPFLAGS += -Wall -Wextra -fno-exceptions -std=c++98 -I./src -g
 
 C_RELEASE_FLAGS = -O3 -march=native -DNDEBUG
 CPP_RELEASE_FLAGS = $(C_RELEASE_FLAGS)
@@ -10,6 +10,6 @@ CPP_RELEASE_FLAGS = $(C_RELEASE_FLAGS)
 C_DEBUG_FLAGS = -O0 -g -gdwarf-2
 CPP_DEBUG_FLAGS = $(C_DEBUG_FLAGS)
 
-LD_FLAGS = -lyaml -lz
+LD_FLAGS += -lyaml -lz
 
 TARGET = sinuca3
